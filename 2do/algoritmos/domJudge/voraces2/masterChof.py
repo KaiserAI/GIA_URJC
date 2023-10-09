0d2ae6f9cf37b9f38517b7a854e7f232bb69b8d5
Kaiser_AI <josewalter2468@gmail.com>
2023-10-09 17:20:53 +0200

    Update masterChof.py

--- 2do/algoritmos/domJudge/voraces2/masterChof.py
+++ 2do/algoritmos/domJudge/voraces2/masterChof.py
@@ -3,23 +3,17 @@ numFoods, sizeBasket = map(int, input().strip().split())
 foods = []
 for i in range(numFoods):
     enter = input().split()
-    foods.append(
-        (int(enter[1]) / int(enter[2]), enter[0], int(enter[1]), int(enter[2]))
-    )
-foods.sort()
+    foods.append((int(enter[2]) / int(enter[1]), enter[0], int(enter[1]), int(enter[2])))
+foods.sort(reverse=True)
 sol = False
 idx = 0
-vectSol = []
 sum = 0
 while not sol:
-    if foods[idx][2] < sizeBasket:
-        vectSol.append(foods[idx][3])
+    if foods[idx][2] > sizeBasket: 
+        sum += foods[idx][3] * (sizeBasket / foods[idx][2])
+        sol = True
+    else:
         sum += foods[idx][3]
         sizeBasket -= foods[idx][2]
         idx += 1
-    else:
-        num = foods[idx][2] / sizeBasket
-        vectSol.append(foods[idx][3] * num)
-        sum += foods[idx][3] * num
-        sol = True
-print(sum)
+print(f"{sum:.6f}")
