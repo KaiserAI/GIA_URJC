741fcd7ab45c517c5ed3a924c0d2b11d1ee8d625
Kaiser_AI <josewalter2468@gmail.com>
2023-10-10 13:15:02 +0200

    Update dumbledora.py

--- 2do/algoritmos/domJudge/voracesGrafos1/dumbledora.py
+++ 2do/algoritmos/domJudge/voracesGrafos1/dumbledora.py
@@ -6,22 +6,27 @@ def updateComponentes(componentes, idOld, idNew):
     return componentes
 
 
+def updateCostPerRoom(costPerRoom, c, h1, h2):
+    costPerRoom[h1] += c
+    costPerRoom[h2] += c
+
+
 def kruskal(numRooms, rooms):
     componentes = list(range(numRooms))
     count = numRooms
     mst = 0
     rooms.sort()
     i = 0
-    listDist = [0]*numRooms
+    costPerRoom = [0]*numRooms
     while count > 1 and len(rooms) > i:
-        dist, h1, h2 = rooms[i]
+        c, h1, h2 = rooms[i]
         if componentes[h1] != componentes[h2]:
             count -= 1
-            mst += dist
-            listDist[h2] += dist
+            mst += c
+            updateCostPerRoom(costPerRoom, c, h1, h2)
             updateComponentes(componentes, componentes[h1], componentes[h2])
         i += 1
-    return mst, listDist
+    return mst, costPerRoom
 
 numRooms, numHalls = map(int, input().strip().split())
 rooms = []
@@ -29,7 +34,8 @@ for _ in range(numHalls):
     h1, h2, c = map(int, input().strip().split())
     rooms.append((c, h1, h2))
 
-mst, listDist = kruskal(numRooms, rooms)
+mst, listCost = kruskal(numRooms, rooms)
 print(f'Coste total: {mst}')
 for i in range(numRooms):
-    print(f'H{i}: {listDist[i]}')
+    print(f'H{i}: {listCost[i]}')
+    
